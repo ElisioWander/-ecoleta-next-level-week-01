4e1df935a0d867333139232a95e49a1d29536dd8
Elisio Wander <elisio741@hotmail.com>
2020-07-24 00:03:07 -0300

    next-level-week-aplication

--- src/database/db.js
+++ src/database/db.js
@@ -4,3 +4,9 @@ const sqlite3 = require("sqlite3").verbose()
 //criar o objeto que irá fazer operações no banco de dados
 
 const db = new sqlite3.Database("./src/database/database.db")
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTES places();
+    `)
+})
